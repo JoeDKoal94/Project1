e66899f302e815e8fab0aed4449a29cfa6e36e3e
JoeDKoal94 <joedkoal94@gmail.com>
2020-11-29 21:43:23 -0600

    Add

--- src/main/java/TRMS/ServiceDriver.java
+++ src/main/java/TRMS/ServiceDriver.java
@@ -22,7 +22,7 @@ TimerDao schedUpdate = new TimerDao("postgres");
 		t.scheduleAtFixedRate(schedUpdate, 0, 30000);
 		Javalin app = Javalin.create( config -> {
 			config.addStaticFiles("/public");
-	}).start(80);
+	}).start(9090);
 		
 		app.post(LOG_IN, ctx -> authController.login(ctx));
 		app.get(LOG_IN, ctx -> authController.checkUser(ctx));
